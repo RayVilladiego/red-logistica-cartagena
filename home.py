e639004f96646f3be5f7a764235a58a6ae4959c1
RayVilladiego <silfredomendoza711@gmail.com>
2025-05-05 09:29:51 -0500

    home.py

--- home.py
+++ home.py
@@ -1,16 +1,11 @@
-# home.py
 import streamlit as st
 
 def show_home():
-    st.title("🏠 Bienvenido a la Red Logística Inteligente")
+    st.title("📦 Sistema de Reparto Inteligente")
     st.markdown("""
-    Esta plataforma permite el seguimiento en tiempo real de entregas, rutas inteligentes, visualización de KPI's y análisis predictivo mediante redes neuronales.
-    
-    ### Funcionalidades principales:
-    - Visualización del **Dashboard logístico**
-    - Seguimiento en tiempo real de pedidos
-    - Predicción de rutas y tiempos estimados
-    - Gestión de pedidos desde un solo lugar
-    
-    Selecciona una opción en el menú lateral para comenzar.
+        Bienvenido al sistema logístico de Cartagena. Desde aquí puedes navegar por las funciones clave:
+        - Seguimiento de pedidos
+        - Predicción de entrega por red neuronal
+        - Rutas recomendadas según condiciones
+        - Dashboard con KPIs y exportación de datos
     """)

1d7b539b4670c39aa9f24ee2ddf8c4a6b8c94ee7
RayVilladiego <silfredomendoza711@gmail.com>
2025-05-24 12:55:57 -0500

    predict.py

--- predict.py
+++ predict.py
@@ -15,25 +15,15 @@ def predict_view():
 
     model, encoder, scaler = load_models()
 
-    encoder_type = type(encoder).__name__
-    st.write("Tipo de encoder cargado:", encoder_type)
-
-    if hasattr(encoder, 'categories_'):
-        # Es OneHotEncoder directamente
-        categorias_zona_destino = encoder.categories_[0]
-        categorias_clima = encoder.categories_[1]
-        categorias_tipo_via = encoder.categories_[2]
-    elif hasattr(encoder, 'transformers_'):
+    # Extraer categorías del encoder (ajusta la posición según como lo guardaste)
+    if hasattr(encoder, 'transformers_'):
         categorias_zona_destino = encoder.transformers_[0][1].categories_[0]
         categorias_clima = encoder.transformers_[0][1].categories_[1]
         categorias_tipo_via = encoder.transformers_[0][1].categories_[2]
-    elif hasattr(encoder, 'transformers'):
-        categorias_zona_destino = encoder.transformers[0][1].categories_[0]
-        categorias_clima = encoder.transformers[0][1].categories_[1]
-        categorias_tipo_via = encoder.transformers[0][1].categories_[2]
     else:
-        st.error("No se pudieron encontrar las categorías en el encoder.")
-        return
+        categorias_zona_destino = encoder.categories_[0]
+        categorias_clima = encoder.categories_[1]
+        categorias_tipo_via = encoder.categories_[2]
 
     hora = st.number_input("Hora de salida (0-23)", min_value=0, max_value=23, value=8)
     dia_semana = st.selectbox(
@@ -47,6 +37,7 @@ def predict_view():
     distancia_km = st.number_input("Distancia (km)", min_value=0.1, max_value=100.0, value=5.0, step=0.1)
 
     if st.button("Predecir"):
+        # El input debe ir en el orden que entrenaste
         X_new = np.array([[hora, dia_semana, zona_destino, clima, tipo_via, distancia_km]])
         try:
             X_encoded = encoder.transform(X_new)
